4dca1ff98e8a38e61721ecb2c70754253ce6ed23
GreenTeaDude <rumancevartem24@gmail.com>
2026-03-29 19:33:19 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,8 @@
-// Copyright 2022 UNN-IASR
-#include "fun.h"
+#ifndef FUN_H
+#define FUN_H
 
-unsigned int faStr1(const char *str) {
-    return 0;
-}
+unsigned int faStr1(const char *str);
+unsigned int faStr2(const char *str);
+unsigned int faStr3(const char *str);
 
-unsigned int faStr2(const char *str) {
-    return 0;
-}
-
-unsigned int faStr3(const char *str) {
-    return 0;
-}
+#endif
